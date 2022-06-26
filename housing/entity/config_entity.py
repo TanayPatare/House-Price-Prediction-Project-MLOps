57bb5ab41b9808e2730b50500d55103757273cf6
TanayPatare <tanaypatare@gmail.com>
2022-06-26 17:13:56 +0530

    update

--- housing/entity/config_entity.py
+++ housing/entity/config_entity.py
@@ -11,22 +11,28 @@ DataIngestionConfig = namedtuple("DataIngestionConfig", ["schema_file_path"])
 # 5.ingested_test_dir is test dataset.
 
 #Entity_2
+DataValidationConfig = namedtuple("DataValidationConfig", ["schema_file_path"])
+
+#Entity_3
 DataTransformationConfig = namedtuple("DataTransformationConfig",
 ["add_bedroom_per_room", "transformed_train_dir", "transformed_test_dir", "preprocessed_object_file_path"])
 
-#Entity_3
-ModelTrainingConfig = namedtuple("ModelTrainingConfig",
+#Entity_4
+ModelTrainerConfig = namedtuple("ModelTrainingConfig",
 ["trained_model_file_path", "base_accuracy"])
 # 1.trained_model_file_path is pickle file location.
 # 2.base_accuracy is accuracy of older ML model.
 
-#Entity_4
+#Entity_5
 ModelEvaluationConfig = namedtuple("ModelEvaluationConfig",
 ["model_evaluation_file_path", "time_stamp"])
 # 1.model_evaluation_file_path is file loaction which has all info of older models.
 # 2.time_stamp has time of file saved.
 
-#entity_5
+#entity_6
 ModelPusherConfig = namedtuple("ModelPusherConfig",
 ["export_dir_path"])
 # 1.if the new model has better accuracy than base accuracy then export_dir_path holds path for that model for replacement of older model.
+
+#Entity_7
+TrainingPipelineConfig = namedtuple("TrainingPipelineConfig", ["artifact_dir"])
